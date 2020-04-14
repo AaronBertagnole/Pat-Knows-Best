739dee525a485742ca3ade5a6fcf29d52c0be40f
Terik Hone <terikhone@gmail.com>
2020-04-13 22:14:15 -0600

    Added comments to the keypress event

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+// On keypress will search api if more then 3 letters are in the search term. Also waits 1 second after keypress
+// before searching to prevent a bunch of ajax calls.
 let keyTimer;
 $("#search").keypress(function () {
   clearTimeout(keyTimer);
