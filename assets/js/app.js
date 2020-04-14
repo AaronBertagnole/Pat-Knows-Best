914e93ab173d70aea73147070ab01eaf3a5befcb
Terik Hone <terikhone@gmail.com>
2020-04-14 00:49:42 -0600

    On Keypress search for board games complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,11 +3,11 @@
 let keyTimer;
 $("#search").keypress(function () {
   clearTimeout(keyTimer);
-  if( $(this).val().length >= 3 ) {
+  if( $(this).val().length >= 2 ) {
     keyTimer = setTimeout(function(){
-      pat.searchVideoGames($("#search").val(), 4).then((data) => {
-        for(let i = 0; i < data.results.length; i++) {
-          console.log(data.results[i].name);
+      pat.searchBoardGames($("#search").val()).then((data) => {
+        for(let i = 0; i < data.games.length; i++) {
+          console.log(data.games[i].thumb_url);
         }
       });
     }, 1000);
--- assets/js/pat.js
+++ assets/js/pat.js
@@ -2,25 +2,14 @@ const pat = {
 
   // Variables For The Pat Class
 
-  /** - METHODS - **/
 
   /**
-   * Searches the rawg.io api for the game provided, along with what platform they would like.
-   * @param {string} searchTerm - The game we are searching for.
-   * @param {number} platform - The platform that we want to search under that we get from platform dropdown.
-   * @param {number} page - The page we want to start on.
-   * @param {number} [limit=10] - The amount of items per page.
-   *
-   * @example
-   * pat.searchVideoGames("Counter-Strike, 4);
-   *
+   * This is the main search method. Searches any api and returns the results in json
+   * @param {string} url - The Url you want to use in the fetch
    * @return {json}
    */
-  async searchVideoGames(searchTerm, platform, page = 1, limit = 10) {
-    const baseUrl = "https://api.rawg.io/api/games";
-    const queryString = "?platforms=" + platform + "&search=" + searchTerm + "&page=" + page + "&page_size=" + limit;
-
-    const response = await fetch(baseUrl + queryString, {
+  async search(url) {
+    const response = await fetch(url, {
       headers: {
         'Content-Type': 'application/json',
         'User-Agent': "Pat Knows Best"
@@ -29,22 +18,50 @@ const pat = {
     return response.json();
   },
 
+  /**
+   * Searches the rawg.io api for the game provided, along with what platform they would like.
+   * @param {string} searchTerm - The game we are searching for.
+   * @param {number} platform - The platform that we want to search under that we get from platform dropdown.
+   * @param {number} [page=1] - The page we want to start on.
+   * @param {number} [limit=10] - The amount of items per page.
+   *
+   * @return {json}
+   */
+  async searchVideoGames(searchTerm, platform, page = 1, limit = 10) {
+    const baseUrl = "https://api.rawg.io/api/games";
+    const queryString = "?platforms=" + platform + "&search=" + searchTerm + "&page=" + page + "&page_size=" + limit;
+
+    return this.search(baseUrl + queryString);
+  },
 
   /**
    * Searches the boardgameatlas.com api for the game provided
    * @param {string} name - The board game we are searching for.
-   * @param {number} [limit=10] - The
+   * @param {number} [limit=10] - The amount of items to return
    * @param {string} [orderBy= - The way the items are ordered by.
    * @param {boolean} [fuzzyMatch=true] - If set to true it helps with typos
+   *
+   * @returns {json}
    */
-  searchBoardGames(name, limit = 10, orderBy = "popularity", fuzzyMatch = true) {
-    const url = "https://www.boardgameatlas.com/api/search";
-    const queryString = { "name" : name, "limit" : limit, "fuzzy_match" : fuzzyMatch}
+  async searchBoardGames(name, limit = 10, orderBy = "popularity", fuzzyMatch = true) {
+    const baseUrl = "https://www.boardgameatlas.com/api/search";
+    const queryString = "?name=" + name+ "&limit=" + limit+ "&fuzzy_match=" + fuzzyMatch + "&client_id=AoMOmUcuiK";
+    console.log(baseUrl + queryString);
+
+   return this.search(baseUrl + queryString);
   },
 
-  // Recommended method to return the recommended video game or board
-  // game with a count arg with a default of 3
-  recommend(count = 3) {
+  /**
+   * Returns Pats recommendations to the user
+   * @param {number} [limit=3] - The amount of items to return
+   *
+   * @returns {json}
+   */
+  async recommendVideoGames(limit = 3) {
+
+  },
+
+  async recommendBoardGames(limit = 3) {
 
   }
 };
