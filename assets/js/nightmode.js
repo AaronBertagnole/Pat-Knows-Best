18fad6e824996ae44735b5f99f0b0fadff7d066b
ASuicidalCow <aaron.bertagnole@gmail.com>
2020-04-17 22:02:25 -0600

    added night mode and changed the appropiate files.

--- assets/js/nightmode.js
+++ assets/js/nightmode.js
@@ -1,36 +1,36 @@
-const nightMode = {
+const lightMode = {
   /**
    * Enables the dark mode.
    */
   enable() {
-    $("body").addClass("night-mode");
-    localStorage.setItem("nightMode", "enabled");
-    $("#night-mode-toggle span").html("Change To Light Mode");
-    $("#logo").attr("src", "assets/images/logo-dark-mode.png");
+    $("body").addClass("light-mode");
+    localStorage.setItem("lightMode", "enabled");
+    $("#light-mode-toggle span").html("Change To Dark Mode");
+    $("#logo").attr("src", "assets/images/logo.png");
   },
 
   /**
    * Disables the dark mode.
    */
   disable() {
-    $("body").removeClass("night-mode");
-    localStorage.setItem("nightMode", "disabled");
-    $("#night-mode-toggle span").html("Change To Dark Mode");
-    $("#logo").attr("src", "assets/images/logo.png");
+    $("body").removeClass("light-mode");
+    localStorage.setItem("lightMode", "disabled");
+    $("#light-mode-toggle span").html("Change To Light Mode");
+    $("#logo").attr("src", "assets/images/logo-dark-mode.png");
   },
 };
 
 
-// Check Local storage for nightMode if it is currently set to enabled then turn on dark mode.
-if(localStorage.getItem("nightMode") === "enabled") {
-  nightMode.enable();
+// Check Local storage for lightMode if it is currently set to enabled then turn on dark mode.
+if(localStorage.getItem("lightMode") === "enabled") {
+  lightMode.enable();
 }
 
 // On Click of the dark mode icon it will toggle between dark and light mode.
-$("#night-mode-toggle").on("click", function() {
-  if(localStorage.getItem("nightMode") === "enabled") {
-    nightMode.disable();
+$("#light-mode-toggle").on("click", function() {
+  if(localStorage.getItem("lightMode") === "enabled") {
+    lightMode.disable();
   } else {
-    nightMode.enable();
+    lightMode.enable();
   }
 });
